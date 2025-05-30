6de6309ca9bc3a5ca527598e7eac9a330b69fd13
pavlosdan <no-reply@example.com>
2025-05-30 08:12:56 -0700

    Create a plugin dedicated .env file in .ddev directory and store the variables there.

--- private-registry/common.sh
+++ private-registry/common.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 #ddev-generated
 
-PR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ENV_FILE="${ENV_FILE:-${PR_DIR}/.env}"
+ENV_FILE="${DDEV_APPROOT}/.ddev/.env.ddev-private-registry"
 
 # load env if it exists
 if [ -f "$ENV_FILE" ]; then
